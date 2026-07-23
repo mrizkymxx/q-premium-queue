aa9df5f21af077bf9209826a9464f7fb94ad65b3
M RIZKY <muhamrizky00@gmail.com>
2026-07-23 15:35:11 +0700

    fix: optimize Vercel build script

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-# Install Flutter SDK
+# Install Flutter SDK (web-only to save time)
 git clone --depth 1 --branch stable https://github.com/flutter/flutter.git /tmp/flutter 2>&1
 export PATH="/tmp/flutter/bin:$PATH"
-flutter precache 2>&1
+flutter config --no-analytics 2>&1
 
-# Build with env vars from Vercel
+# Build web with env vars from Vercel
 flutter build web --release \
   --dart-define=SUPABASE_URL="$SUPABASE_URL" \
   --dart-define=SUPABASE_ANON_KEY="$SUPABASE_ANON_KEY" 2>&1
