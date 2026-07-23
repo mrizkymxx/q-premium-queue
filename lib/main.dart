876f58db2f11145d5ff51aa4f2dcdc84e4e8e99b
M RIZKY <muhamrizky00@gmail.com>
2026-07-23 23:07:37 +0700

    fix(web): enable usePathUrlStrategy for deep linking

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'config/supabase_config.dart';
 import 'app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  usePathUrlStrategy();
   await initializeDateFormatting('id', null);
   await SupabaseConfig.initialize();
   runApp(const QPremiumApp());
